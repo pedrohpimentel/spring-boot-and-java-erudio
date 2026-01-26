543c4de603c741a17dfe38b919274b036a081349
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-01-26 06:15:24 -0300

    feat: Retornando Listas de Objetos em JSON

--- spring-boot-and-java-erudio/src/main/java/br/com/erudio/spring_boot_and_java_erudio/service/PersonService.java
+++ spring-boot-and-java-erudio/src/main/java/br/com/erudio/spring_boot_and_java_erudio/service/PersonService.java
@@ -3,6 +3,8 @@ package br.com.erudio.spring_boot_and_java_erudio.service;
 import br.com.erudio.spring_boot_and_java_erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -22,6 +24,26 @@ public class PersonService {
         person.setAddress("Monte Sião - Minas Gerais");
         person.setGender("Male");
 
+        return person;
+    }
+
+    //Lembre-se, o método "findAll" sempre vai retornar uma lista (List<>).
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname: " + i);
+        person.setLastName("Lastname: " + i);
+        person.setAddress("Some Address in Brazil " );
+        person.setGender("Male");
         return person;
     }
 
