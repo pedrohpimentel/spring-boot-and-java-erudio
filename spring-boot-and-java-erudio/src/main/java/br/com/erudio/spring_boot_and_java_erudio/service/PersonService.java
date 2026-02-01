00cc46cf671dc633f96f822518d952ccb2223c7f
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-02-01 09:16:23 -0300

    feat: Substituindo os Mocks do Service pelo acesso ao Repositório

--- spring-boot-and-java-erudio/src/main/java/br/com/erudio/spring_boot_and_java_erudio/service/PersonService.java
+++ spring-boot-and-java-erudio/src/main/java/br/com/erudio/spring_boot_and_java_erudio/service/PersonService.java
@@ -1,6 +1,9 @@
 package br.com.erudio.spring_boot_and_java_erudio.service;
 
+import br.com.erudio.spring_boot_and_java_erudio.exception.ResourceNotFoundException;
 import br.com.erudio.spring_boot_and_java_erudio.model.Person;
+import br.com.erudio.spring_boot_and_java_erudio.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,52 +17,59 @@ public class PersonService {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
-    public Person findById(String id){
-        logger.info("Finding one Person! ");
+    //Injetando a dependência "PersonRepository" no PersonService
+    @Autowired
+    PersonRepository repository;
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Pedro");
-        person.setLastName("Pimentel");
-        person.setAddress("Monte Sião - Minas Gerais");
-        person.setGender("Male");
-
-        return person;
-    }
-
-    //Lembre-se, o método "findAll" sempre vai retornar uma lista (List<>).
+    //Quem irá prover o método findAll será o Spring Data.
     public List<Person> findAll(){
         logger.info("Finding all Person! ");
 
-        List<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            Person person = mockPerson(i);
-            persons.add(person);
-        }
-        return persons;
+       return repository.findAll();
     }
 
-    private Person mockPerson(int i) {
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Firstname: " + i);
-        person.setLastName("Lastname: " + i);
-        person.setAddress("Some Address in Brazil " );
-        person.setGender("Male");
-        return person;
+    //Em findById ele irá retornar um Optional
+    public Person findById(Long id){
+        logger.info("Finding one Person! ");
+
+        //Em caso de não encontrar id, será lançada a exceção criada "ResourceNotFoundException"
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
     }
 
     public Person create(Person person){
         logger.info("Creating one Person! ");
-        return person;
+        return repository.save(person);
     }
 
+    //O update irá substituir os mocks que foi antigamente.
     public Person update(Person person){
         logger.info("Updating one Person! ");
-        return person;
+
+        //Irá receber a mesma estrutura do "findById", mas ele será um novo "Person" que se chamará entity;
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        //Depois chamaremos os parâmetros pelo entity;
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        //Retornando um novo "person";
+        return repository.save(person);
     }
 
-    public void delete(String id){
+    //Para o delete mesma lógica.
+    public void delete(Long id){
         logger.info("Deleting one Person! ");
+
+        //Iremos colocar ele dentro de um "Person" que se chamará entity;
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        //Ao final ele irá deletar (entity).
+        repository.delete(entity);
     }
 }
+
