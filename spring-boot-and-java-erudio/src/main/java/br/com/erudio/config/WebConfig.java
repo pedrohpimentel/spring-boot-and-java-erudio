f0a310b9eeb330b6f170dd6d3078264feb8589a9
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-02-10 05:06:08 -0300

    feat: Content Negotiation via HEADER Parameter

--- spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
+++ spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/WebConfig.java
@@ -13,9 +13,20 @@ public class WebConfig implements WebMvcConfigurer {
 
         // Via EXTENSION. http://localhost:8080/api/person/v1/2.xml or http://localhost:8080/api/person/v1/4.JSON Deprecated on Spring Boot 2.6
         // Via QUERY PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
+        /*
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+
+    }*/
+
+    // Via HEADER PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
